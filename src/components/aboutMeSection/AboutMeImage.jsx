5bfc0ccf333f15d28f2becda0e8c47d379f8d4a9
Rajanjha12 <rajanjha122004@gmail.com>
2025-05-06 01:31:20 +0530

    first

--- src/components/aboutMeSection/AboutMeImage.jsx
+++ src/components/aboutMeSection/AboutMeImage.jsx
@@ -1,14 +1,17 @@
 const AboutMeImage = () => {
   return (
     <div className="h-[500px] w-[300px] relative">
+      {/* Image container */}
       <div className="h-[500px] w-[300px] rounded-[100px] absolute overflow-hidden">
-      <img
-      src="/images/about-me.jpg" // this references public/images/about-me.jpg
-      alt="About Me"
-      
-    />
+        <img
+          src="/images/about-me.jpg" // correct path if in public/images
+          alt="About Me"
+          className="object-cover h-full w-full"
+        />
       </div>
-      <div className="h-[500px] w-[250px] bg-orange absolute bottom-[-30px] left-[-30px] rounded-bl-[120px] rounded-tr-[120px] rounded-br-[20px] rounded-tl-[20px] -z-10"></div>
+
+      {/* Decorative background box */}
+      <div className="h-[500px] w-[250px] bg-orange-500 absolute bottom-[-30px] left-[-30px] rounded-bl-[120px] rounded-tr-[120px] rounded-br-[20px] rounded-tl-[20px] -z-10"></div>
     </div>
   );
 };
